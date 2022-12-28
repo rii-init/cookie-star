ad340abb6cc41725d7e00985e82bb46fa48eed0f
ultr7a.com <ultr7a@gmail.com>
2022-12-28 18:27:18 -0500

    static linker for static pages; re-add api server;

---
+++ client/static-linker.sh
@@ -0,0 +1,5 @@
+pages=$(find . | grep -E "public/.*/index.htm")
+
+while read page; do
+    sed -i "s|/static/js/main.js|$(cat build/index.html | grep -E -o '/static/js/main.*\.js')|"  "$page"
+done <"$pages"
