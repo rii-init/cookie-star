4e1407e611975ea93121b9f297dd4b5afadc6f27
ultr7a.com <ultr7a@gmail.com>
2022-12-31 15:12:04 -0500

    defer loading of content on landing page

--- client/static-linker.sh
+++ client/static-linker.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
 function get_bundle_src() {
-      echo "$(cat build/index.html | grep -E -o "/static/$1/main.*\.$1")"
+      echo `cat build/index.html | grep -E -o "/static/$1/main.*\.$1"`
 }
 
 # Identify pages to static link:
@@ -12,28 +13,22 @@ mv build/minimal.css build/static/css/minimal.css;
 
 
 # # Modify Main Page:      
+
 # Get    Element of Main JS Bundle: 
-main_js=$(cat build/index.html | grep -E -o  '<script.*src="(.*main.*)"></script>')
+main_js=$(cat build/index.html | grep -Eo "main\..*\.js");
+echo "Linking: Main Page: main_js== $main_js";
+
 ## Remove Element of Main JS Bundle from <head>:
-sed -i  "s|<script defer=\"defer\" src=\"$main_js\"></script>||" build/index.html;
+sed -i  "s|<script defer=\"defer\" src=\"/static/js/$main_js\"></script>||" build/index.html;
 ## Add    Element of Main JS Bundle to   <body>:
-sed -i  "s|<js_async/>|<script src=\"$main_js\"></script>|"  build/index.html;
+sed -i  "s|<js_async/>|<script src=\"/static/js/$main_js\"></script>|"  build/index.html;
+
 
 # Get    Element of Main CSS Bundle:
-main_css=$(cat build/index.html | grep -E -o  '<link href="(.*main.*\.css)" rel="stylesheet">')
+main_css=$(cat build/index.html | grep -Eo  'main\..*\.css');
+echo "Linking: Main Page: main_css== $main_css";
+
 ## Remove Element of Main CSS Bundle from <head>:
-sed -i  "s|<link href=\"$main_css\" rel=\"stylesheet\">||" build/index.html;
+sed -i  "s|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">||" build/index.html;
 ## Add    Element of Main CSS Bundle to   <body>:
-sed -i  "s|<css_async/>|<link href=\"$main_css\" rel=\"stylesheet\">|"                  build/index.html;
-
-for page in $pages
-do
-      
-      echo "Linking page: [ $page ]";
-      
-      # link JS bundle:
-      sed -i "s|/static/js/main.js|$(get_bundle_src js)|"     "$page";
-      # Link CSS bundle:
-      sed -i "s|/static/css/main.css|$(get_bundle_src css)|"  "$page";
-
-done
+sed -i  "s|<css_async/>|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">|"                  build/index.html;
