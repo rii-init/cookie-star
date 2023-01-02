089526f5abbc1e576d9517096171d516a85fdec4
ultr7a.com <ultr7a@gmail.com>
2023-01-01 20:42:36 -0500

    server_side rendering

--- client/linker.content.sh
+++ client/linker.content.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
+CLIENT_PATH="$PWD";
+cd src/400_scene;
 
 # Identify static pages to copy jsx content into:
-cd src/400_scene;
 pages="$(find . | grep -E ".*/.*.tsx")"
 
 # Copy JSX content into static pages:
 for page in $pages
 do
-      
+    
     echo "Linking Content: [ $page ]";
-          
-    HTML_NAME=$(echo $page | grep -Eo "^([a-zA-Z0-9_]+)/]");
-    JSX_NAME=$(echo  $page | grep -Eo "^[a-zA-Z0-9_]+/([a-zA-Z0-9_]+)\.tsx");
+    echo "🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈"
 
+    HTML_NAME=$(echo $page | grep -Eo "([a-zA-Z0-9_]+)" | head -n 1);
+    JSX_NAME=$(echo $page  | grep -Eo "([a-zA-Z0-9_]+)" | tail -n 2 | head -n 1);
 
-    echo "HTML_NAME: $HTML_NAME";
-    echo "JSX_NAME: $JSX_NAME";
-    echo ".'* <3 .'* ";
-    echo "";
-
+    # Get JSX content:
+    heading_text="$(cat "$HTML_NAME/$JSX_NAME.tsx" | grep -Eo "<h1>.*</h1>" )" 
+    main_text="$(awk '/<main>/,/<\/main>/' "$HTML_NAME/$JSX_NAME.tsx" )"    
 
     # # Copy [[JSX content] heading] into [[static page] heading]:
-    # sed -i "s|<h1 data-jsx-h1></h1>|$(cat $JSX_NAME.tsx | grep -Eo "" )|" "../400_scene/$HTML_NAME/index.html;"
+    # # Copy [[JSX content] main]    into [[static page] main]:
+    
+    HTML_PATH="$CLIENT_PATH/public/$HTML_NAME/index.html";
 
-    # # Copy [[JSX content] main] into [[static page] main]:
-    # sed -i "s|<div data-jsx-main></div>|$(cat $JSX_NAME.tsx | grep -Eo "" )|" "../400_scene/$HTML_NAME/index.html;"
-  
+    if [ "home" == "$HTML_NAME" ]; then
+        HTML_PATH="$CLIENT_PATH/public/index.html";
+    fi
 
+    
+    echo "HTML_PATH == $HTML_PATH";
+
+    sed -i "s|<h1 data-jsx-h1></h1>|$heading_text|"  "$HTML_PATH"
+
+    echo "MAIN_TEXT == $main_text";
+    echo "$main_text" > "MAIN_TEXT_FILE"
+
+    awk '/<div data-jsx-main><\/div>/{system("cat MAIN_TEXT_FILE");next}1' "$HTML_PATH" > "$HTML_PATH"
+
+    #sed -i "s|<div data-jsx-main></div>|$main_text|" "$HTML_PATH"
+    
 done
+
+# # 🐶🐱🐰🦊🐨🐯🦁🐧🐦🐤🐥🦆🦢🦅🦉🦚🦎🐬🐟🐠🦈🐚🦀🦐
