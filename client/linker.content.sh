1c82eddef7b68420e944f2424cf51a44b7ddaa0f
ultr7a.com <ultr7a@gmail.com>
2023-01-03 00:38:52 -0500

    bring templates to a clean state

--- client/linker.content.sh
+++ client/linker.content.sh
@@ -22,21 +22,19 @@ do
     heading_text="$(cat "$HTML_NAME/$JSX_NAME.tsx" | grep -Eo "<h1>.*</h1>" )" 
     main_text="$(awk '/<main>/,/<\/main>/' "$HTML_NAME/$JSX_NAME.tsx" )"    
 
-    # # Copy [[JSX content] heading] into [[static page] heading]:
-    # # Copy [[JSX content] main]    into [[static page] main]:
-    
+ 
     HTML_PATH="$CLIENT_PATH/build/$HTML_NAME/index.html";
 
     if [ "home" == "$HTML_NAME" ]; then
         HTML_PATH="$CLIENT_PATH/build/index.html";
-        sed -i "s|</body>||" "$HTML_PATH";
-        sed -i "s|</html>||" "$HTML_PATH";
+        # sed -i "s|</body>||" "$HTML_PATH";
+        # sed -i "s|</html>||" "$HTML_PATH";
     fi
 
     sed -i "s|<h1 data-jsx-h1></h1>|$heading_text|"  "$HTML_PATH"
 
-    echo "$main_text"    >> "$HTML_PATH";
-    echo "$bottom_bread" >> "$HTML_PATH";
+    # echo "$main_text"    >> "$HTML_PATH";
+    # echo "$bottom_bread" >> "$HTML_PATH";
     
     sed -i "s|className|class|" "$HTML_PATH";
     
