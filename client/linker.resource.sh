4f22105eab0749880f04c05e89170670b4ff2ab3
ultr7a.com <ultr7a@gmail.com>
2023-01-03 09:20:02 -0500

    use closing tags for parameter markers

--- client/linker.resource.sh
+++ client/linker.resource.sh
@@ -18,13 +18,13 @@ echo "Linking: Main Page: main_css== $main_css";
 ## Remove Element of Main JS Bundle from <head>:
 sed -i  "s|<script defer=\"defer\" src=\"/static/js/$main_js\"></script>||" build/index.html;
 ## Add    Element of Main JS Bundle to   <body>:
-sed -i  "s|<js_async/>|<script src=\"/static/js/$main_js\"></script>|"  build/index.html;
+sed -i  "s|<js_async><js_async/>|<script src=\"/static/js/$main_js\"></script>|"  build/index.html;
 
 
 ## Remove Element of Main CSS Bundle from <head>:
 sed -i  "s|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">||" build/index.html;
 ## Add    Element of Main CSS Bundle to   <body>:
-sed -i  "s|<css_async/>|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">|"                  build/index.html;
+sed -i  "s|<css_async><css_async/>|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">|"                  build/index.html;
 
 # Identify pages to static link:
 pages="$(find . | grep -E "build/.*/index.htm")"
@@ -36,9 +36,9 @@ do
       echo "Linking page: [ $page ]";
       
       #Link JS bundle:
-      sed -i "s|<js_async/>|<script src=\"/static/js/$main_js\"></script>|" $page
+      sed -i "s|<js_async><js_async/>|<script src=\"/static/js/$main_js\"></script>|" $page
 
       #Link CSS bundle:
-      sed -i "s|<css_async/>|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">|"  "$page";
+      sed -i "s|<css_async><css_async/>|<link href=\"/static/css/$main_css\" rel=\"stylesheet\">|"  "$page";
 
 done
