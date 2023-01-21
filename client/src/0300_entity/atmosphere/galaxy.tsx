2e3e41e1de2e2ad0e001e1b59c08c1a28608868d
ultr7a.com <ultr7a@gmail.com>
2023-01-21 18:08:46 -0500

    use instancing to improve performance of distant meshes

--- client/src/0300_entity/atmosphere/galaxy.tsx
+++ client/src/0300_entity/atmosphere/galaxy.tsx
@@ -1,23 +1,49 @@
+import { useLayoutEffect, useRef } from "react";
+import { InstancedMesh, Object3D, Quaternion, Vector3 } from "three";
+import { Universe } from "../../0000_concept/universe";
 import { Star } from "../../0100_element/100_circle/cylinder/manifold/star"
 
+
+
 export let Galaxy = () => {
-    return (
-        <group>
-            {
-                new Array(63).fill(0).map((_, i) => {
-                    const theta = Math.random() * Math.PI,
-                          phi   = Math.random() * Math.PI;
+  
+    const meshRef = useRef<InstancedMesh>(null);
 
-                    return (
-                        <Star key={i} position={[
-                            Math.sin(theta) * Math.cos(phi) * 100,
-                            Math.sin(theta) * Math.sin(phi) * 100,
-                            Math.cos(theta) * 100,
-                        ]}></Star>
-                    )
+    useLayoutEffect( () => {
+        const tempObject = new Object3D();
+        const center     = new Vector3(0,0,0);
+        
+        for (let y = 0; y < 320; y++) {
+            const theta = Math.random() * Math.PI,
+                  phi   = Math.random() * Math.PI * 2;
 
-                })   
-            }    
-        </group>
-    )
+            tempObject.position.set(
+                Math.sin(theta) * Math.cos(phi) * 100,
+                Math.sin(theta) * Math.sin(phi) * 100,
+                Math.cos(theta) * 100
+            );
+
+            tempObject.lookAt(center);
+
+            tempObject.scale.set(1, 1, 1)
+            tempObject.updateMatrix()
+            
+    
+            if (meshRef && meshRef.current) { 
+                meshRef.current.setMatrixAt(y, tempObject.matrix);
+                meshRef.current.instanceMatrix.needsUpdate = true 
+            }
+        }
+    }, [meshRef.current]);
+    
+
+return (
+  <instancedMesh
+    ref={meshRef}
+    args={[undefined, undefined, 320]}
+  >
+    <boxBufferGeometry attach="geometry" args={[1, 1, 1]}></boxBufferGeometry>
+    <meshBasicMaterial color={Universe.colors.accent} />
+  </instancedMesh>
+)
 }
