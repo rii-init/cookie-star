b1eb0b3a645a3364235ae55608c046b4c603314c
ultr7a.com <ultr7a@gmail.com>
2023-02-08 02:13:41 -0500

    init spritesheet for emojis

--- client/src/0200_component/flat/typography/emoji.ts
+++ client/src/0200_component/flat/typography/emoji.ts
@@ -9,7 +9,29 @@ const iconMap = [
     '💾💿📀📁📂🗂️  ',
 ]
 
-export const getIconCoords = (emoji: string, res = 64) => {
+
+export function createIconCanvas(resolution: [number, number] = [64, 64]) {
+    const canvas = document.createElement('canvas');
+    canvas.width = resolution[0] * 8;
+    canvas.height = resolution[1] * 8;
+    const ctx = canvas.getContext('2d');
+    if   (ctx) {
+          ctx.font = `${resolution[0]}px serif`;
+    }
+    // draw iconMap 
+    iconMap.forEach((row, y) => {
+        row.split('').forEach((emoji, x) => {
+            if (ctx) {
+                ctx.fillText(emoji, x * resolution[0], y * resolution[1]);
+            }
+        })
+    });
+    
+    return canvas;
+}
+
+
+export function getIconCoords(emoji: string, res = 64) {
 
     const yCoordinate = iconMap.indexOf(
             iconMap.filter(
