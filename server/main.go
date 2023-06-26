0a11ef4cfa1c06e808c20fcb02cebf1530a134ef
ultr7a.com <ultr7a@gmail.com>
2023-06-26 19:02:52 -0400

    use labstack/echo instead of gin

--- server/main.go
+++ server/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
+	"path"
 
-	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-
 	"github.com/gorilla/websocket"
+
+	"github.com/labstack/echo"
 )
 
 var upgrader = websocket.Upgrader{} // use default options
@@ -63,74 +60,31 @@ func socketAPI(w http.ResponseWriter, r *http.Request) {
 // var db *gorm.DB
 
 func main() {
-	fmt.Println("Starting ultr7a.com on port 3080")
+	e := echo.New()
 
-	// db = database.InitDb()
+	// Custom route handler for serving index.html for requested folders
+	e.GET("/*", func(c echo.Context) error {
+		// Get the requested path
+		reqPath := c.Request().URL.Path
 
-	r := setupRouter()
-	_ = r.Run(":3080")
-}
+		// Check if the requested path is a directory
+		if info, err := os.Stat("../client/build" + reqPath); err == nil && info.IsDir() {
+			// Construct the full path to index.html
+			indexFilePath := path.Join("../client/build"+reqPath, "index.html")
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-
-	r.Use(IndexHTMLHandler())
-
-	r.GET("/api/socket", gin.WrapF(socketAPI))
-
-	r.POST("/api/dev-console", func(c *gin.Context) {
-		// print out the request body:
-		log.Println(c.Request.Body)
-		// write the response into a file in append mode
-		f, err := os.OpenFile("journal/owo.pages", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Println(err) // 0w0 ..What's dis?
-		}
-		defer f.Close()
-
-		buf := new(bytes.Buffer)
-		buf.ReadFrom(c.Request.Body)
-
-		if _, err := f.WriteString(buf.String() + ",\n"); err != nil {
-			log.Println(err)
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": "ok",
-		})
-
-	})
-
-	r.NoRoute(gin.WrapH(http.FileServer(http.Dir("../client/build"))))
-
-	return r
-}
-
-// IndexHTMLHandler is a custom middleware that serves index.html inside the requested path without a trailing slash
-func IndexHTMLHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		requestPath := c.Request.URL.Path
-
-		// Check if the request path ends with a trailing slash
-		requestPath = strings.TrimSuffix(requestPath, "/")
-		indexDirPath := filepath.Join("../client/build", requestPath)
-
-		fmt.Println("Index DIR: " + indexDirPath)
-		// Check if the path corresponds to a directory
-		if fileInfo, err := os.Stat(indexDirPath); err == nil && fileInfo.IsDir() {
-			// Serve the index.html file inside the directory
-			indexFilePath := filepath.Join(indexDirPath, "index.html")
-
-			fmt.Println("Index File: " + indexFilePath)
-
+			// Check if index.html exists
 			if _, err := os.Stat(indexFilePath); err == nil {
-				fmt.Println("*** Serving index.html ***")
-				c.File(indexFilePath)
-				c.Abort()
-				return
+				// Serve index.html
+				return c.File(indexFilePath)
 			}
 		}
 
-		// Continue to the next middleware or route handler
-		c.Next()
-	}
+		// Serve the requested file or directory using the built-in Echo handler
+		return echo.WrapHandler(http.FileServer(http.Dir("../client/build")))(c)
+	})
+
+	// Start the server
+	e.Start(":3080")
+	// db = database.InitDb()
+
 }
