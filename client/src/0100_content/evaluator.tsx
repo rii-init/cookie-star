f233a3b0f903f9ed171a6623445358e4f8081ad2
ultr7a.com <ultr7a@gmail.com>
2023-08-16 18:43:08 -0400

    proof of concept markdown -> react-three/fiber

--- client/src/0100_content/evaluator.tsx
+++ client/src/0100_content/evaluator.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react';
-import { extend } from '@react-three/fiber'
 
 import { Parser } from "./parser";
-
 import { TextSpan } from '../0200_component/flat/typography/span';
-import { TextH1 } from '../0200_component/flat/typography/h1';
-
-import { Atmosphere } from '../0300_entity/atmosphere/atmosphere';
 import { LinkSurface } from '../0200_component/flat/scalar/LinkSurface';
-import { TextH3 } from '../0200_component/flat/typography/h3';
+import { TextH1 } from '../0200_component/flat/typography/h1';
 import { TextH2 } from '../0200_component/flat/typography/h2';
-import { TextDiv } from '../0200_component/flat/typography/div';
-import { UniverseContext } from '../App';
-import { Sequence } from '../0100_element/200_sequence/sequence';
+import { TextH3 } from '../0200_component/flat/typography/h3';
+import { TextH4 } from '../0200_component/flat/typography/h4';
 import { TextP } from '../0200_component/flat/typography/p';
+import { TextDiv } from '../0200_component/flat/typography/div';
+import { Atmosphere } from '../0300_entity/atmosphere/atmosphere';
 
 
-extend({ TextSpan, TextDiv, TextP, TextH1, TextH2, TextH3, LinkSurface, Sequence, Atmosphere });
+// extend({ TextSpan, TextDiv, TextP, TextH1, TextH2, TextH3, LinkSurface, Sequence, Atmosphere });
 
 interface TextChildNode {
     nodeName: '#text';
@@ -151,34 +147,91 @@ export function Evaluator (props: { location: string }) {
     console.log("the page changed. Time to load the content! :D",
     "btw: the page is:  ", props.location);
 
-    const components = [] as React.ReactNode[];
-    const body = document.body;
-
-
-    if (body) {
-        // iterate over body:
-        for (let i = 0; i < body.childNodes.length; i++) {
-            const node = body.childNodes[i];
-
-            
-            // ignore stuff that isn't static html content:
-            // if (node.nodeName === "#text" && /^\n+$/.test(node.nodeValue || "")) {
-            //     continue;
-            // }
-            
-            if  (   node instanceof HTMLElement && 
-                    (    
-                        node.nodeName           === "SCRIPT" ||
-                        node.getAttribute("id") === "root" 
-                    )
-                )
-            { continue; }
-
-            evaluate(node as HTMLElement, components)
-        }
-    }
-
     return (
-        <>{ components }</>
+        <>
+        {  
+           Array.from(document.body.childNodes)
+                .filter((node) => {
+                    return  (
+                                node instanceof HTMLElement && 
+                                !(    
+                                    node.nodeName           === "SCRIPT" ||
+                                    node.getAttribute("id") === "root" 
+                                 )
+                            )
+                            ||
+                            !/^\n+$/.test(node.nodeValue || "");
+                })    
+                .map((node) => {
+
+                    // It's not uncommon for there to be attributes:
+                    // They might be a data structure that has to be parsed:
+                    if (node.nodeName === '#text') {
+                        return (
+                            <TextSpan>(node as HTMLElement).textContent</TextSpan>
+                        )
+                    }    
+                    
+                    const attrs = Parser.parse(node as HTMLElement)
+
+                    switch (node.nodeName) {
+                        case "SPAN":
+                            return (
+                                <TextSpan {...attrs}>{(node as HTMLElement).textContent}</TextSpan>
+                            )
+
+                        case "H1":
+                            return (<TextH1 {...attrs}>{(node as HTMLElement).textContent}</TextH1>)
+                        case "H2":
+                            return (<TextH2 {...attrs}>{(node as HTMLElement).textContent}</TextH2>)
+                        case "H3":
+                            return (<TextH3 {...attrs}>{(node as HTMLElement).textContent}</TextH3>)
+                        case "H4":
+                            return (<TextH4 {...attrs}>{(node as HTMLElement).textContent}</TextH4>)
+
+                        case "A":
+                            return <LinkSurface location={attrs.location}>{(node as HTMLElement).textContent}</LinkSurface>
+
+                        // If someone has the audacity to use a p tag or a div, they might be doing something complicated,
+                        // so we're going to check if there's more elements inside of it:
+    
+                        case "P":
+                                // testing...
+                                return  node.childNodes.length > 0 
+                                                        ?  node.childNodes[0].nodeName === "ATMOSPHERE" 
+                                                            ? <Atmosphere /> 
+                                                            : <TextP { ...attrs}>{(node as HTMLElement).textContent}</TextP> 
+                                                        : <TextP { ...attrs}>{(node as HTMLElement).textContent}</TextP>
+
+                                    // Array.from((node as HTMLElement).childNodes)
+                                    //     .filter((node) => {
+                                    //         return  (
+                                    //                     node instanceof HTMLElement && 
+                                    //                     !(    
+                                    //                         node.nodeName           === "SCRIPT" ||
+                                    //                         node.getAttribute("id") === "root" 
+                                    //                     )
+                                    //                 )
+                                    //                 ||
+                                    //                 !/^\n+$/.test(node.nodeValue || "");
+                                    //     })
+                                    //     .map((node) => {
+
+                                    //     })
+
+                                    // Oh wait haha.. i need another component for this to be recursive :p
+                                
+                                
+
+                        case "DIV":
+                            return <TextDiv { ...attrs}>
+                                { /*** one thing at a time.. ***/}
+                            </TextDiv>
+                            
+                    }
+                })
+
+        }
+        </>
     )
 }
