50bd87325fb56cb461fe6d9b8a3df904bdd247af
J. R. Evans <ultr7a@gmail.com>
2024-02-14 21:00:53 -0500

    make cloud.formation nicer

--- client/src/0300_entity/atmosphere/cloud.formation.tsx
+++ client/src/0300_entity/atmosphere/cloud.formation.tsx
@@ -2,34 +2,41 @@ import { Vector3 } from "three";
 import { Cloud } from "../../0100_element/x10_fluid/manifold/cloud"
 
 export let CloudFormation = () => {
-    const cloudCenter = [-10, 9, -12];
-    let        coords = [3, 
-                            9, 
-                        -10+Math.random()*-3];
+    const zero3 = new Vector3(0, 0, 0);
+
+    let previousRadius = 0;
+    let previousPosition: Vector3;
+    let           coords = [  3, 9, -25+Math.random()*-3];
 
     return (
         <>
            {
             new Array(7).fill(0).map((_, i) => {
-                const delta = [cloudCenter[0] - coords[0], 0, cloudCenter[2] - coords[2]];
+                const radius = 4+Math.random()*4;
+                const position = (previousPosition === undefined 
+                                    ? zero3 
+                                    : previousPosition).add(
 
-                coords = [coords[0] - delta[0] / 4, 10, coords[2] - delta[2] / 4];
+                                 new Vector3((-0.5 + Math.random()) * 1.3, 
+                                              -0.5 + Math.random(),
+                                             (-0.5 + Math.random()) * 1.3)
+                                            .normalize()
+                                            .multiplyScalar((radius + previousRadius) / 1.5 )
+                                    );
 
-                const radius = 4+Math.random()*8;
+                if (Math.random() > (0.8 - i/40)) {
+                    position.multiplyScalar(1.5);
+                }
+                
+                previousRadius = radius;
+                previousPosition = position;
 
-                const nextPosition = new Vector3(Math.random(),
-                                    Math.random(),
-                                    Math.random()
-                                    )
-                            .normalize()
-                            .multiplyScalar(radius/2)
                 return (
-                    <Cloud key={i}
-                        radius={radius} 
-                      position={
-                        new Vector3(coords[0], coords[1], coords[2])
-                   .add(nextPosition).toArray()
-                      }></Cloud>
+                    <Cloud key={i} radius={radius} 
+                      position={     new Vector3(coords[0], coords[1], coords[2])
+                                            .add(position).toArray()
+                               }>
+                    </Cloud>
                 )
             })
            }
